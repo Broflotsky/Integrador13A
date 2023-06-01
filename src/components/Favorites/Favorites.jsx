753cc8d64d9507f8f3d8e32ce9b135c21dd2750c
Ivan Reyes <vinacelo@gmail.com>
2023-06-01 18:04:13 -0500

    last integration done M2

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Favorites.module.css";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import Card from "../Card/Card";
+import { filterCards, orderCards } from "../../redux/actions";
 
 const Favorites = (props) => {
   const { myFavorites } = props;
+  const dispatch = useDispatch();
+  const [aux, SetAux] = useState(false);
+
+  const handleOrder = (e) => {
+    dispatch(orderCards(e.target.value));
+    SetAux(!aux);
+  };
+
+  const handleFilter = (e) => {
+    dispatch(filterCards(e.target.value));
+  };
 
   return (
     <div>
+      <select onChange={handleOrder}>
+        <option value="A">Ascendente</option>
+        <option value="D">Descendente</option>
+      </select>
+
+      <select onChange={handleFilter}>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+        <option value="Genderless">Genderless</option>
+        <option value="unknown">unknown</option>
+      </select>
+
       {myFavorites.map((char) => {
         return (
           <Card
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,14 @@
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 }
 
 const rootReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case "ADD_FAV":
-            let copy1 = state.myFavorites;
+            let copy1 = state.allCharacters;
             copy1.push(payload)
-            return { ...state, myFavorites: copy1 }
+            return { ...state, myFavorites: copy1, allCharacters: copy1 }
 
         case "REMOVE_FAV":
             let copy2 = state.myFavorites.filter((char) => {
@@ -15,6 +16,20 @@ const rootReducer = (state = initialState, { type, payload }) => {
             })
             return { ...state, myFavorites: copy2 }
 
+        case 'FILTER':
+            let copy3 = state.allCharacters.filter((char) => {
+                return char.gender === payload
+            })
+            return { ...state, myFavorites: copy3 }
+
+        case 'ORDER':
+            let copy4 = state.allCharacters.sort((a, b) => {
+                return payload === 'A' ? a.id - b.id : b.id - a.id
+            });
+            return {
+                ...state,
+                myFavorites: copy4
+            }
         default:
             return state;
     }
